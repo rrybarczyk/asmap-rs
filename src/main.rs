aeaf661fdede01920da32a5f2fce232a7848ed67
RJ Rybarczyk <rj@rybar.tech>
2020-01-06 14:36:33 -0500

    Clippy lint

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ pub(crate) use crate::common::*;
 
 fn main() -> Result<(), Error> {
     let path = "./data/dump_01_2019-12-28";
-    let input = File::open(path).unwrap();
+    // let input = File::open(path).unwrap();
+    let input = File::open(path).map_err(|error| Error::IoError { io_error: error })?;
     let buffered = BufReader::new(input);
 
     let mut prefix_to_as_paths: HashMap<Address, HashSet<Vec<u32>>> = HashMap::new();
@@ -15,22 +16,20 @@ fn main() -> Result<(), Error> {
     for line in buffered.lines() {
         let l = line.unwrap();
         let bgp_line = BGPPath::from_str(&l)?;
-        prefix_to_as_paths.entry(bgp_line.addr)
+        prefix_to_as_paths
+            .entry(bgp_line.addr)
             .or_insert_with(HashSet::new)
             .insert(bgp_line.as_path);
     }
 
     let mut prefix_to_common_suffix: HashMap<&Address, Vec<u32>> = HashMap::new();
     for (prefix, as_paths) in prefix_to_as_paths.iter() {
-        let mut as_paths_sorted: Vec<&Vec<u32>> = as_paths
-            .iter()
-            .collect();
+        let mut as_paths_sorted: Vec<&Vec<u32>> = as_paths.iter().collect();
         as_paths_sorted.sort_by(|a, b| a.len().cmp(&b.len())); // descending
         let mut rev_common_suffix: Vec<u32> = as_paths_sorted[0].to_vec();
         rev_common_suffix.reverse();
-        for as_path in as_paths_sorted
-            .iter()
-            .skip(1) { // first one is already in rev_common_suffix
+        for as_path in as_paths_sorted.iter().skip(1) {
+            // first one is already in rev_common_suffix
             let mut rev_as_path: Vec<u32> = as_path.to_vec();
             rev_as_path.reverse();
 
@@ -46,7 +45,9 @@ fn main() -> Result<(), Error> {
             }
         }
         rev_common_suffix.reverse();
-        prefix_to_common_suffix.entry(prefix).or_insert(rev_common_suffix);
+        prefix_to_common_suffix
+            .entry(prefix)
+            .or_insert(rev_common_suffix);
     }
     Ok(())
 }
