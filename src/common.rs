f1cfacfbfd46c43fcd86a6003751095e94c76897
RJ Rybarczyk <rj@rybar.tech>
2020-01-06 15:16:41 -0500

    Cargo fmt

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,7 @@
 pub(crate) use crate::{address::Address, bgp_path::BGPPath, error::Error};
+pub(crate) use std::collections::{HashMap, HashSet};
 pub(crate) use std::fs::File;
 pub(crate) use std::io::{BufRead, BufReader};
-pub(crate) use std::collections::{HashMap, HashSet};
 pub(crate) use std::{
     fmt::{self, Display, Formatter},
     net::IpAddr,
